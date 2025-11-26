eaddc792af27270abb5406de4552faee33c9097d
Viktor Berke <github.bviktor@outlook.com>
2025-11-26 03:16:40 +0100

    Allow for setting the charge order Fixes #16

--- assassinCharges.ts
+++ assassinCharges.ts
@@ -32,16 +32,68 @@ const chargesJsonDir = "hd\\overlays\\common";
 // filename prefix
 // charge icon prefix
 // entity ID 1
-// entity ID 2 for level 1/2/3
-const chargeTypes: string[][]=
+// entity ID 2 for level 1
+// entity ID 2 for level 2
+// entity ID 2 for level 3
+// charge order index starting from 1
+var chargeTypes: string[][]=
 [
-	[ "other", "phnx", "201238954", "201238954", "1037577841", "1018592639" ],
-	[ "lightning", "claw", "2217683865", "2217683865", "2181987240", "3339063977" ],
-	[ "cold", "cold", "2337602525", "2846492227", "3682896650", "1077384344" ],
-	[ "damage", "tiger", "304314862", "304314862", "1174273717", "2010153596" ],
-	[ "steal", "cobra", "1322619066", "1322619066", "2158892417", "1591210754" ],
-	[ "fire", "fire", "2846492227", "2846492227", "3682896650", "1077384344" ]
+	[ "other", "phnx", "201238954", "201238954", "1037577841", "1018592639", "1" ],
+	[ "cold", "cold", "2337602525", "2846492227", "3682896650", "1077384344", "2" ],
+	[ "steal", "cobra", "1322619066", "1322619066", "2158892417", "1591210754", "3" ],
+	[ "lightning", "claw", "2217683865", "2217683865", "2181987240", "3339063977", "4" ],
+	[ "fire", "fire", "2846492227", "2846492227", "3682896650", "1077384344", "5" ],
+	[ "damage", "tiger", "304314862", "304314862", "1174273717", "2010153596", "6" ]
 ]
+
+// Override JS array sorting to adjust charge icon order by user preference
+// This is great because it cannot break if the user sets duplicate values
+function sortCharges(a: string[], b: string[])
+{
+	var aIndex = Number(a[6]);
+	var bIndex = Number(b[6]);
+
+	if (aIndex === bIndex)
+	{
+		return 0;
+	}
+	else
+	{
+		return (aIndex < bIndex) ? -1 : 1;
+	}
+}
+
+// Print charge order for debugging
+// @ts-ignore
+function printChargeOrder()
+{
+	var chargeOrder = "";
+
+	for (var i = 0; i < chargeTypes.length; i++)
+	{
+		chargeOrder += i + 1 + ". " + chargeTypes[i][0] + " (" + (chargeTypes[i][1]) + ") ";
+	}
+
+	console.log(chargeOrder);
+}
+
+// Apply icon order if set
+for (var i = 0; i < chargeTypes.length; i++)
+{
+	// array is indexed from 0, our icons from 1
+	var y = i + 1;
+
+	if (Object.hasOwn(config, "ach_icon" + y))
+	{
+		// It complains that the value can be null, but it really can't
+		// @ts-ignore
+		var index = chargeTypes.findIndex(arr => arr.includes(config["ach_icon" + y].toString()));
+		chargeTypes[index][6] = y.toString();
+	}
+}
+
+// Reorder the charges array accordingly
+chargeTypes.sort(sortCharges);
 
 // Sprite positioning
 if (Object.hasOwn(config, "ach_ypos") && config["ach_ypos"])
