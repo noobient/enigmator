4cf696d192129e2e1776a945ca660abbd5d8c3cf
Viktor Berke <github.bviktor@outlook.com>
2025-11-26 07:07:05 +0100

    Option to hide potions / scrolls Fixes #18

--- mod.ts
+++ mod.ts
@@ -154,6 +154,48 @@ itemNames.forEach((item) =>
 				newName = `ÿc;100% ÿc0Rejuvenation Potion`;
 			}
 			break;
+		case "isc": // Scroll of Identify
+		case "tsc": // Scroll of Town Portal"
+			if (config["ift_scroll"])
+			{
+				newName = "";
+			}
+			break;
+		case "gpl": // Strangling Gas Potion
+		case "gpm": // Choking Gas Potion
+		case "gps": // Rancid Gas Potion
+		case "hp1": // Minor Healing Potion
+		case "hp2": // Light Healing Potion
+		case "hp3": // Healing Potion
+		case "hp4": // Greater Healing Potion
+		case "mp1": // Minor Mana Potion
+		case "mp2": // Light Mana Potion
+		case "mp3": // Mana Potion
+		case "mp4": // Greater Mana Potion
+		case "opl": // Fulminating Potion
+		case "opm": // Exploding Potion
+		case "ops": // Oil Potion
+		case "rvs": // Rejuvenation Potion
+		case "vps": // Stamina Potion
+		case "wms": // Thawing Potion
+		case "yps": // Antidote Potion
+			if(config["ift_potion"])
+			{
+				newName = "";
+			}
+			break;
+		case "hp5": // Super Healing Potion
+			if(config["ift_superhealing"])
+			{
+				newName = "";
+			}
+			break;
+		case "mp5": // Super Mana Potion
+			if(config["ift_supermana"])
+			{
+				newName = "";
+			}
+			break;
 		case "cm1": // Small Charm
 			newName = item.enUS + ` ÿc5[ÿc:•ÿc5]`;
 			break;
